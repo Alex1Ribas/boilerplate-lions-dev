44d5de1530c47a67bd582a73f87a08f550e29179
alex ribas <alex.ribas12@gmail.com>
2025-11-26 09:17:20 -0300

    feat: add integration test for user repository deleteById method

---
+++ src/__tests__/integration/repository/user.delete-by-delete-by-id.int.test.js
@@ -0,0 +1,17 @@
+import userModel from "../../../models/user.model.js";
+import userRepository from "../../../repositories/user.repository.js";
+
+describe('when we try delete a user by ID',  () => {
+    it('return a sucessfully user deleted ', async() =>{
+        const payload = {
+            name: 'usertest ',
+            email: `userdelete-${Date.now()}@example.com`,
+            password: 'wrongpasswordtobedeleted12@34'        
+        }
+
+        const create = await userModel.create(payload)
+        const find = await userRepository.deleteById(create._id)
+
+        expect(find.lenght === 0)
+    })
+})
