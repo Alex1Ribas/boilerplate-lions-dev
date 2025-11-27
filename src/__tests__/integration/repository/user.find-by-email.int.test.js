e7b988784510e74d7bc661bf5078216020749413
alex ribas <alex.ribas12@gmail.com>
2025-11-27 11:26:53 -0300

    test: simplify user find by email integration test and improve assertions

--- src/__tests__/integration/repository/user.find-by-email.int.test.js
+++ src/__tests__/integration/repository/user.find-by-email.int.test.js
@@ -1,12 +1,6 @@
 import userModel from "../../../models/user.model";
 import userRepository from "../../../repositories/user.repository";
-beforeAll( async () =>{
-    userModel.init()
-})
 
-beforeEach(async() =>{
-    userModel.deleteMany([])
-})
 describe('when we try find corret user by email', () =>{
     it('should return corret user', async () =>{
         const payload = {
@@ -18,7 +12,6 @@ describe('when we try find corret user by email', () =>{
         const create = await userModel.create(payload);
         const find = await userRepository.findByEmail(create.email)
 
-        expect(find).toBeDefined()
-        expect(console.log(find))
+        expect(find.email).toEqual(create.email)
     })
 })
